21f6ce3a8432b5cbc6b807ea89579c86c3093dba
vertin <13746245+vertin5015@user.noreply.gitee.com>
2025-05-18 21:38:39 +0800

    fix: 修改vite设定以构建项目

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import HomePage from "./routes/homePage/homePage.jsx";
-import CreatePage from "./routes/createPage/createPage.jsx";
-import PostPage from "./routes/postPage/postPage.jsx";
-import AuthPage from "./routes/authPage/authPage.jsx";
-import SearchPage from "./routes/searchPage/searchPage.jsx";
-import ProfilePage from "./routes/profilePage/profilePage.jsx";
-import MainLayout from "./routes/layouts/mainLayout.jsx";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "./routes/layouts/mainLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const HomePage = React.lazy(() => import("./routes/homePage/homePage"));
+const CreatePage = React.lazy(() => import("./routes/createPage/createPage"));
+const PostPage = React.lazy(() => import("./routes/postPage/postPage"));
+const ProfilePage = React.lazy(() =>
+  import("./routes/profilePage/profilePage")
+);
+const SearchPage = React.lazy(() => import("./routes/searchPage/searchPage"));
+const AuthPage = React.lazy(() => import("./routes/authPage/authPage"));
 
 const queryClient = new QueryClient();
 
